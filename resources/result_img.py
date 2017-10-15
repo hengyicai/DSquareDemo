d40df5de2a475c2659f524946b605bc2664296b4
hengyi <hengyicai@hotmail.com>
2017-10-15 16:57:33 +0800

    Allow user to custom the blend_function and blend_opacity

--- resources/result_img.py
+++ resources/result_img.py
@@ -12,16 +12,30 @@ import img_process.r4_image_blend_0723
 import uuid
 from util import util
 import cv2
+from blend_modes import blend_modes
+import inspect
 
 
 class ResultImg(Resource):
+    def __init__(self):
+        self.__init__()
+        self.ACCEPTED_BLEND_FUNCTIONS = [item[1] for item in
+                                         inspect.getmembers(blend_modes, predicate=inspect.isfunction)]
+
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('origin_uuid', type=str, location='args', required=True)
         parser.add_argument('user_prefer_uuid', type=str, location='args', required=True)
+        parser.add_argument('blend_function', type=str, location='args', required=True)
+        parser.add_argument('blend_opacity', type=float, location='args', required=True)
         args = parser.parse_args()
         origin_img_uuid = args["origin_uuid"]
         user_prefer_img_uuid = args["user_prefer_uuid"]
+        blend_function = args["blend_function"]
+        blend_opacity = args["blend_opacity"]
+
+        if blend_function not in self.ACCEPTED_BLEND_FUNCTIONS or not 0 <= blend_opacity <= 1:
+            abort(404)
 
         origin_img_path = os.path.join(setting.ORIGIN_IMGS_ROOT, str(origin_img_uuid) + '.jpg')
         user_prefer_img_path = os.path.join(setting.USER_PREFER_IMGS_ROOT, str(user_prefer_img_uuid) + '.jpg')
@@ -29,7 +43,9 @@ class ResultImg(Resource):
         if os.path.isfile(origin_img_path) and os.path.isfile(user_prefer_img_path):
             final_img = img_process.r4_image_blend_0723.blend_imgs(
                 origin_img_path,
-                user_prefer_img_path
+                user_prefer_img_path,
+                blend_function=getattr(blend_modes, blend_function),
+                blend_opacity=blend_opacity
             )
 
             final_img_uuid = str(uuid.uuid4().hex)
